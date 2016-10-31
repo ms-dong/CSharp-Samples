69fa9cc782f75467ddd8ed39e7f5eb00ca2ab7df
Dong Xu <dongx@microsoft.com>
2016-10-31 10:55:52 +0800

    add a test function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApplication
 {
@@ -7,6 +8,13 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static Stream ByteToStream(Byte[] byteArray)
+        {
+            //MemoryStream stream = new MemoryStream();
+            //stream.Write(byteArray, 0, byteArray.Length);
+            return new MemoryStream(byteArray);
         }
     }
 }
