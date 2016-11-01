ecc897d1dfe66da50435b767b23a9de3b38c419a
dongx <dongx@DONGX-WORKPLACE>
2016-11-01 16:06:51 +0800

    update test

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
 
         public static Stream ByteToStream(Byte[] byteArray)
